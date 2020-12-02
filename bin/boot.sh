7fc1ca544e55c3b1a093514a79e17d8d7d8bf6e5
Dennis Mende <programming.dmende@hotmail.com>
2020-12-03 00:20:43 +0100

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -9,4 +9,4 @@ touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 (tail -f -n 0 $HOME/logs/nginx/*.log &)
 
 echo "load nginx conf"
-exec $HOME/vendor/nginx/sbin/nginx
+exec $HOME/vendor/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
